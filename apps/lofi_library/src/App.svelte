31a58129e8b6cec5b26df299353583f58cc3721d
qwertypoi2 <laura.hudson76@gmail.com>
2026-02-08 15:05:16 +0000

    created visual components for lofi library

---
+++ apps/lofi_library/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import GameGrid from './GameGrid.svelte';
+</script>
+
+<main>
+  <h1>Lofi Library</h1>
+  <GameGrid />
+</main>
+
+<style>
+  main { text-align: center; color: #5d576b; background-color: #f0e4d7; min-height: 100vh; }
+</style>
